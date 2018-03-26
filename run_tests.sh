373b69e07d06b8e79e6dba177bf15320efe3fffe
pascalwhoop <accounts@pascalbrokmeier.de>
2018-03-26 22:53:26 +0200

    testrunner script

---
+++ run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+COUNTER=1
+echo "Watching file tree for changed files. Running tests when it occurs"
+python -m unittest
+inotifywait -rm --exclude "py~|__pycache__|venv\/|swp|git\/" -e close_write ./ | while read change; do
+    echo "change detected"
+    python -m unittest
+    COUNTER=$[$COUNTER +1]
+    echo "run $COUNTER"
+    echo $change
+done
